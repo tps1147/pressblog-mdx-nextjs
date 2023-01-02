d4b9355665465b1679e71f77e9dfa650d6d62165
tps1147 <tps1147@gmail.com>
2023-01-01 20:14:19 -0500

    main

--- components/MDXComponents.js
+++ components/MDXComponents.js
@@ -6,6 +6,10 @@ import Image from 'next/image';
 const MDXComponents = {
   p: (props) => <p {...props} className={styles.p} />,
   a: (props) => <a {...props} className={styles.link} />,
+  ul: (props) => <ul {...props} className={styles.ul} />,
+  li: (props) => <li {...props} className={styles.li} />,
+  blockquote: (props) => <blockquote {...props} className={styles.blockquote} />,
+  inlineCode: (props) => <code {...props} className={styles.inlineCode} />,
   h1: (props) => <h1 {...props} className={styles.postTitle} />,
   img: (props) => <img className={styles.mainImage} {...props} />,
   code: (props) => <code {...props} className={styles.code} />,
